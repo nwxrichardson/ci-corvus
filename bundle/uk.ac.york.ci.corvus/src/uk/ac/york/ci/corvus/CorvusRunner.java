fd4715a81c23680ccdeca0778a831817d6609763
N.W.Richardson <nr823@york.ac.uk>
2025-10-12 13:32:05 +0100

    Move to IApplication CorvusRunner

---
+++ bundle/uk.ac.york.ci.corvus/src/uk/ac/york/ci/corvus/CorvusRunner.java
@@ -0,0 +1,72 @@
+package uk.ac.york.ci.corvus;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.sirius.business.api.dialect.DialectManager;
+import org.eclipse.sirius.business.api.query.DViewQuery;
+import org.eclipse.sirius.business.api.resource.ResourceDescriptor;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.business.api.session.factory.SessionFactory;
+import org.eclipse.sirius.common.tools.api.resource.ImageFileFormat;
+import org.eclipse.sirius.diagram.DSemanticDiagram;
+import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
+import org.eclipse.sirius.ui.business.api.dialect.ExportFormat;
+import org.eclipse.sirius.ui.business.api.dialect.ExportFormat.ExportDocumentFormat;
+import org.eclipse.sirius.ui.business.api.dialect.ExportResult;
+import org.eclipse.sirius.ui.business.api.session.UserSession;
+import org.eclipse.sirius.viewpoint.DRepresentation;
+import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
+
+public class CorvusRunner implements IApplication {
+
+	private IProgressMonitor progressMonitor;
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		context.applicationRunning();
+		Map<String, Object> contextArguments = context.getArguments();
+		return run(contextArguments.get(IApplicationContext.APPLICATION_ARGS));
+	}
+
+	private Object run(Object argsArray) {
+		 // Get session from an absolute path (not in a workspace)
+        URI sessionResourceURI = URI.createFileURI("C:/Users/nr823/git/CI-Corvus/bundle/uk.ac.york.ci.corvus/psl.aird");
+        Session session = SessionManager.INSTANCE.getExistingSession(sessionResourceURI);
+		try {
+			
+			session = SessionFactory.INSTANCE.createSession(sessionResourceURI, new NullProgressMonitor());
+			session.open(new NullProgressMonitor());
+	        DViewQuery query = new DViewQuery(session.getOwnedViews().iterator().next());
+	        DRepresentationDescriptor representationDesc = query.getLoadedRepresentationsDescriptors().get(0);
+	        DRepresentation representation = representationDesc.getRepresentation();
+	        System.out.println(representation);
+//	         Export it as SVG image
+	        ExportFormat exportFormat = new ExportFormat(ExportDocumentFormat.NONE, ImageFileFormat.PNG);
+	        System.out.println(DialectUIManager.INSTANCE.canExport(exportFormat));
+	        ExportResult exp = DialectUIManager.INSTANCE.exportWithResult(representation, session, new Path("C:/Users/nr823/git/psl-ci/psl.example/image.png"), exportFormat,
+			        new NullProgressMonitor());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		}
+
+	@Override
+	public void stop() {
+		if (progressMonitor != null) {
+			progressMonitor.setCanceled(true);
+		}
+	}
+
+}
