703963b53c7062b74edceee76ab84741d6bcb91f
N.W.Richardson <56395605+nwxrichardson@users.noreply.github.com>
2025-10-12 13:27:10 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,11 @@
 
 LAUNCHERJAR=$(ls -1 /epsilon/plugins/org.eclipse.equinox.launcher_*.jar | head -1)
 WORKSPACE="/workspace/"
-ANT_FILE="$1"
-ANT_TARGET="$2"
-
-# We might be reading the ant file from stdin
-if [[ "${ANT_FILE}" == "--" ]]
-then
-	ANT_FILE="${WORKSPACE}/build.xml"
-	# Use cat to read stdin and write it to file
-	cat > "${ANT_FILE}"
-fi
 
 # Run the ant script
 xvfb-run java -ea -jar "${LAUNCHERJAR}" \
 	-data "${WORKSPACE}" \
-	-application org.eclipse.ant.core.antRunner \
-	-buildfile "${ANT_FILE}" "${ANT_TARGET}"
+	-application uk.ac.york.ci.corvus.CorvusRunner \
 
 # If there was an error, read the Eclipse logs and exit
 if [[ $? -gt 0 ]]
